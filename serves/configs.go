967d29c3cbd22acf6a52da4a0d8388153f31870a
深蓝 <mail@ri-co.cn>
2020-11-17 16:11:55 +0800

    初始化数据库

---
+++ serves/configs.go
@@ -0,0 +1,17 @@
+/*Package serves 配置文件
+用于区分本地开发和服务器部署，顺便可以保密
+*/
+package serves
+
+//是否为本地开发环境
+var dev = true
+
+//SQLConfg 数据库信息
+func SQLConfg() string {
+	var local = "root:123456@/mym2m"
+	var serve = ""
+	if dev {
+		return local
+	}
+	return serve
+}
