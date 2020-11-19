3df3711c90db61d0da465c9987ac0eeaac00524d
深蓝 <mail@ri-co.cn>
2020-11-19 09:01:31 +0800

    重构mqtt文件

--- handlers/mqtt.go
+++ handlers/mqtt.go
@@ -10,35 +10,67 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-//MqttRes 处理接收到的mqtt数据
-func MqttRes() {
+var client mqtt.Client
+
+var deal mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func init() {
 	mqtt.ERROR = log.New(os.Stdout, "", 0)
-	opts := mqtt.NewClientOptions().AddBroker("tcp://39.96.49.63:1883").SetClientID("emqx_test_client")
+	opts := mqtt.NewClientOptions().AddBroker("tcp://mqtt.ri-co.cn:1883").SetClientID("golang_back")
 
 	opts.SetKeepAlive(600 * time.Second)
 	// 设置消息回调处理函数
-	opts.SetDefaultPublishHandler(f)
-	opts.SetPingTimeout(1 * time.Second)
+	opts.SetDefaultPublishHandler(deal)
+	opts.SetPingTimeout(5 * time.Second)
 
-	c := mqtt.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
+	client = mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
+	//调用接收函数
+	MqttRes()
+}
+
+//MqttRes 处理接收到的mqtt数据
+func MqttRes() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	//接收端
 	go func() {
-		if token := c.Subscribe("testtopic/#", 0, nil); token.Wait() && token.Error() != nil {
+		if token := client.Subscribe("testtopic/#", 0, nil); token.Wait() && token.Error() != nil {
 			fmt.Println(token.Error())
 			os.Exit(1)
 		}
 	}()
-	//发送端
+
+	wg.Wait()
+}
+
+//MqttSend 发送mqtt数据
+func MqttSend(topic, msg string) {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		token := c.Publish("testtopic/gosend", 0, false, "嗯哼~从go发出的")
+		token := client.Publish(topic, 0, false, msg)
 		token.Wait()
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+//MqttSendDefault 发送mqtt数据，使用默认topic：gosend
+func MqttSendDefault(msg string) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		token := client.Publish("gosend", 0, false, msg)
+		token.Wait()
+		wg.Done()
 	}()
 	wg.Wait()
 }
